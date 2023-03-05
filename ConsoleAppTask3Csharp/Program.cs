dd9c10d379d3273cdc692be62450546c3fb74479
ElWin95 <elvinma@code.edu.az>
2023-03-05 22:09:11 +0400

    'test'

--- ConsoleAppTask3Csharp/Program.cs
+++ ConsoleAppTask3Csharp/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace ConsoleAppTask3Csharp
+﻿using System.Xml;
+
+namespace ConsoleAppTask3Csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int len;
+            l1:
+            Console.Write("Telebelerin sayini daxil et: ");
+            if (!int.TryParse(Console.ReadLine(), out len) || len <= 0)
+            {
+                Console.WriteLine("Say duzgun daxil edilmeyib");
+                goto l1;
+            }
+            StudentInfo[] students = new StudentInfo[len];
+
+            students[0] = new StudentInfo();
+            students[0].name= "Elvin";
+            students[0].surname= "Mustafayev";
+            students[0].age= 28;
+            students[0].speciality= "Fizik";
+            students[0].groupNo= "Fz34";
+
+            StudentInfo student2 = new StudentInfo();
+            student2.name = "Ilkin";
+            student2.surname = "Yusifov";
+            student2.age = 29;
+            student2.speciality = "Fizik";
+            student2.groupNo = "Fz34";
+            students[1] = student2;
+
+            students[2] = new StudentInfo()
+            {
+                name = "dart",
+                surname = "this is pro langs"
+            };
+           
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                Console.WriteLine(students[i]);
+            }
+
+            //StudentInfo student1 = new StudentInfo();
+            //student1.name = "Elvin";
+            //student1.surname = "Mustafayev";
+            //student1.age = 28;
+            //student1.speciality = "Fizik";
+            //student1.groupNo = "Fz34";
+
+            //Console.WriteLine(student1);
+            //Console.WriteLine(student2);
         }
     }
 }
---
+++ ConsoleAppTask3Csharp/StudentInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTask3Csharp
+{
+    internal class StudentInfo
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public string speciality;
+        public string groupNo;
+
+        public override string ToString()
+        {
+            return $"{name} {surname} {age} {speciality} {groupNo}";
+        }
+    }
+}
