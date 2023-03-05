51546774d3f920ef5dd3ea95ff7b443214592e5d
ElWin95 <elvinma@code.edu.az>
2023-03-06 00:01:13 +0400

    'gecikme'

---
+++ ConsoleAppTask3Csharp/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTask3Csharp
+{
+    internal class Helper
+    {
+        static public string ReadString(string question)
+        {
+        l1:
+            Console.Write(question);
+            string value = Console.ReadLine();
+            if (string.IsNullOrEmpty(value))
+            {
+                goto l1;
+            }
+            return value;
+        }
+    }
+}
--- ConsoleAppTask3Csharp/Program.cs
+++ ConsoleAppTask3Csharp/Program.cs
@@ -14,28 +14,35 @@ namespace ConsoleAppTask3Csharp
                 Console.WriteLine("Say duzgun daxil edilmeyib");
                 goto l1;
             }
+
             StudentInfo[] students = new StudentInfo[len];
 
-            students[0] = new StudentInfo();
-            students[0].name= "Elvin";
-            students[0].surname= "Mustafayev";
-            students[0].age= 28;
-            students[0].speciality= "Fizik";
-            students[0].groupNo= "Fz34";
-
-            StudentInfo student2 = new StudentInfo();
-            student2.name = "Ilkin";
-            student2.surname = "Yusifov";
-            student2.age = 29;
-            student2.speciality = "Fizik";
-            student2.groupNo = "Fz34";
-            students[1] = student2;
-
-            students[2] = new StudentInfo()
+            for (int i = 0; i < students.Length; i++)
             {
-                name = "dart",
-                surname = "this is pro langs"
-            };
+                StudentInfo student = new StudentInfo();
+
+                student.name= Helper.ReadString($"{i+1}. Ad: ");
+                student.surname= Helper.ReadString($"{i+1}. Soyad: ");
+
+                Console.Write($"{i + 1}. Soyadd: ");
+                student.surname = Console.ReadLine();
+                int age;
+            l2:
+                Console.Write($"{i + 1}. Yas: ");
+                string strAge = Console.ReadLine();
+                if (!int.TryParse(strAge, out age) || age < 17)
+                {
+                    Console.WriteLine($"{strAge} uygun yas deyil!");
+                    goto l2;
+                }
+                Console.Write($"{i + 1}. Ixtisas: ");
+                student.speciality = Console.ReadLine();
+                Console.Write($"{i + 1}. Qrup nomresi: ");
+                student.groupNo = Console.ReadLine();
+                students[i] = student;
+            }
+
+            Console.WriteLine("===========================");
            
             for (int i = 0; i < students.Length; i++)
             {
